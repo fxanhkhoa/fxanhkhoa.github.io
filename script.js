8e5362bb4096345a3300575fdf2b5a2a70982ecc
Khoa <khoa@wearebloom.com.au>
2023-03-10 17:47:28 +0700

    add something

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let activeNav = null;
+let activeNav = "home-nav";
+
+document.addEventListener("DOMContentLoaded", () => {
+	navClick("home-nav");
+});
 
 function navClick(name) {
 	if (activeNav) {
@@ -6,8 +10,9 @@ function navClick(name) {
 		activeElement.classList.remove("nav-active");
 	}
 	activeNav = name;
-  const activeElement = document.getElementById(activeNav);
-  activeElement.classList.add("nav-active");
+	const activeElement = document.getElementById(activeNav);
+	activeElement.classList.add("nav-active");
 	if (name === "home-nav") {
+		location.href = "#";
 	}
 }
