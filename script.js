11ca1edac3ae1d616787cc4118d7d5787190c6a0
Khoa <khoa@wearebloom.com.au>
2023-05-08 18:05:35 +0700

    update home section

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
 function navClick(name) {
 	if (activeNav) {
 		const activeElement = document.getElementById(activeNav);
-		activeElement.classList.remove("nav-active");
+		activeElement.classList.remove("cta-active");
 	}
 	activeNav = name;
 	const activeElement = document.getElementById(activeNav);
-	activeElement.classList.add("nav-active");
+	activeElement.classList.add("cta-active");
 	if (name === "home-nav") {
 		location.href = "#";
 	}
